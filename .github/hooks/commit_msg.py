9ed1fe88acc4b4d06683cdf8ee989e4f542f2e07
rocio <rociocpoppe@gmail.com>
2023-07-10 21:45:44 -0300

    feat: PLZ-9 create hooks for git commands

---
+++ .github/hooks/commit_msg.py
@@ -0,0 +1,30 @@
+import sys
+
+
+def main():
+    with open(sys.argv[1], "r", encoding="utf-8") as f_p:
+
+        commit_prefix = ('fix:', 'feat:', 'refactor:', 'test:')
+
+        lines = f_p.readlines()
+
+        for commit_msg in lines:
+
+            if commit_msg[0] == "#":
+                continue
+            if commit_msg.startswith(commit_prefix):
+                print("Commit message is valid")
+                print(commit_msg)
+                return sys.exit(0)
+            else:
+                print(
+                    f"""Commit error: The commit message should start with 
+                    {','.join(commit_prefix)}'"""
+                )
+
+    return sys.exit(1)
+
+
+
+if __name__ == "__main__":
+    main()
