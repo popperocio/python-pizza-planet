0eff902ab37b772fd6611941ba54e3cbc163d2f5
rocio <rociocpoppe@gmail.com>
2023-07-11 14:07:40 -0300

    test: PlZ-2 add tests for get by id and get all for beverage controller and add fixtures for those tests

--- app/test/controllers/test_beverage.py
+++ app/test/controllers/test_beverage.py
@@ -28,3 +28,28 @@ def test_update(app, beverage: dict):
         pytest.assume(updated_beverage[param] == value)
         pytest.assume(beverage_from_database[param] == value)
 
+
+def test_get_by_id(app, beverage: dict):
+    created_beverage, _ = BeverageController.create(beverage)
+    beverage_from_db, error = BeverageController.get_by_id(created_beverage['_id'])
+    pytest.assume(error is None)
+    for param, value in created_beverage.items():
+        pytest.assume(beverage_from_db[param] == value)
+    assert len(created_beverage) == len(beverage_from_db)
+    
+
+
+def test_get_all(app, beverages: list):
+    created_beverages = []
+    for beverage in beverages:
+        created_beverage, _ = BeverageController.create(beverage)
+        created_beverages.append(created_beverage)
+
+    beverages_from_db, error = BeverageController.get_all()
+    searchable_beverages = {db_beverage['_id']: db_beverage for db_beverage in beverages_from_db}
+    pytest.assume(error is None)
+    for created_beverage in created_beverages:
+        current_id = created_beverage['_id']
+        assert current_id in searchable_beverages
+        for param, value in created_beverage.items():
+            pytest.assume(searchable_beverages[current_id][param] == value)
