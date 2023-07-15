02cea1e38c1e0c369b3563431966e467c217ae43
rocio <rociocpoppe@gmail.com>
2023-07-14 23:30:08 -0300

    feat: PlZ-8 refactor services

--- app/services/order.py
+++ app/services/order.py
@@ -1,30 +1,22 @@
 from app.common.http_methods import GET, POST
+from .base_service import BaseService
 from flask import Blueprint, jsonify, request
 
 from ..controllers import OrderController
 
 order = Blueprint('order', __name__)
-
+service = BaseService(OrderController)
 
 @order.route('/', methods=POST)
 def create_order():
-    order, error = OrderController.create(request.json)
-    response = order if not error else {'error': error}
-    status_code = 200 if not error else 400
-    return jsonify(response), status_code
+    return service.create_entity()
 
 
 @order.route('/id/<_id>', methods=GET)
 def get_order_by_id(_id: int):
-    order, error = OrderController.get_by_id(_id)
-    response = order if not error else {'error': error}
-    status_code = 200 if order else 404 if not error else 400
-    return jsonify(response), status_code
+    return service.get_entity_by_id(_id)
 
 
 @order.route('/', methods=GET)
 def get_orders():
-    orders, error = OrderController.get_all()
-    response = orders if not error else {'error': error}
-    status_code = 200 if orders else 404 if not error else 400
-    return jsonify(response), status_code
+    return service.get_entities()
